4e2801d97d6dd4b4594482a2744c0767aa344c3e
nikae <nikae2018@gmail.com>
2023-11-14 11:13:44 +0700

    5.1 Создание основной формы приложения

--- app/src/main/java/com/example/task5/MainActivity.kt
+++ app/src/main/java/com/example/task5/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.task5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.RadioGroup
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var editNumberCount: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
